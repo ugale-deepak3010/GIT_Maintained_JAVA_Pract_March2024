046c3d579ecdaac8bdc4b332c5e4ca14edf00f3b
Dipak Ugale <dipak.ugale@corrigo.com>
2024-03-09 13:02:57 +0530

    new advance java practices project added.

---
+++ CORE_JAVA/Advance-Java-Practices-Oct-2023-master/Advance-Java-Practices-Oct-2023-master/src/Dcom/AdvanceJava/_Collections/C6_Collections.java
@@ -0,0 +1,32 @@
+package Dcom.AdvanceJava._Collections;
+
+/*
+Ordering
+Duplicates
+Speed
+Memory
+
+1.	All types of collections are implements Iterable methods! 
+				it have foreach method.
+2.	Collection interface which extends Iterable.
+ 		add, remove, isEmpty, toArray..
+ 		
+3. There is no classes directly. There is another layer of iterfaces.
+		Set: No duplicate. unordered
+		Map: No duplicate. unordered.  Key:Value pair.
+				Does not extends Collection Interface but Still Collection..
+				
+		List: Allow duplicate. Ordered.
+		Queue: FIFO
+		
+		
+
+ */
+public class C6_Collections{
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
